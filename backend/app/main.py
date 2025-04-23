b64d128dbc4008df7bf8277304b49cb320e084b3
Leandro Oliveira <leandro.gooli@gmail.com>
2025-04-23 20:08:10 +0000

    Feat: endpoint para gerar-projetos

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,39 @@
 from fastapi import FastAPI 
+from typing import List, Optional
+from pydantic import BaseModel
+import requests
+from app.services.crewai import CrewAiService
 
-app = FastAPI()
+app = FastAPI(
+    app_name="CodeSpark",
+    description="Uma plataforma de desenvolvimento de projetos guiada por IA"
+    )
 
+
+client = CrewAiService()
+
+class ProjetoRequest(BaseModel):
+    areas: List[str]
+    tecnologias: str
+    descricao: str
+    usar_exa: Optional[bool] = False
 
 @app.get("/health")
 async def health_check():
     return {"status": "Tudo correto"}
 
 
-
 @app.get("/")
 async def root():
     return {"messagem": "Bem vindo ao backend do Codesprint"}
+
+
+@app.post("/gerar-projeto")
+async def gerar_projeto(req: ProjetoRequest):
+    resultado = client.gerar_projeto(
+        areas=req.areas,
+        tecnologias=req.tecnologias,
+        descricao=req.descricao,
+        usar_exa=req.usar_exa or False 
+        )
+    return resultado
